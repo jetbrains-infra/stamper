b254ce566a5c3272cdd676582766c6c54aded3ba
Nikita Ashikhmin <veoring@gmail.com>
2017-08-12 23:52:55 +0300

    Change oauth server to Jetbrains Hub In jetbrains everybody use Hub oauth server. We should provide the ability to use own oauth servers, determed as params in application.yaml file.

--- src/main/kotlin/ru/jetbrains/testenvrunner/TestEnvRunnerApplication.kt
+++ src/main/kotlin/ru/jetbrains/testenvrunner/TestEnvRunnerApplication.kt
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import java.net.Socket
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 @EnableScheduling
 @EnableOAuth2Sso
@@ -25,5 +29,44 @@ class TestEnvRunnerApplication : WebSecurityConfigurerAdapter() {
 }
 
 fun main(args: Array<String>) {
+    val trustAllCerts = disableSalSilicateCheck()
+
+    val sslContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory())
     SpringApplication.run(TestEnvRunnerApplication::class.java, *args)
 }
+
+private fun disableSalSilicateCheck(): Array<TrustManager> {
+    val trustAllCerts = arrayOf<TrustManager>(object : X509ExtendedTrustManager() {
+
+        override fun checkClientTrusted(x509Certificates: Array<X509Certificate>, s: String) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(x509Certificates: Array<X509Certificate>, s: String) {
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate>? {
+            return null
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(x509Certificates: Array<X509Certificate>, s: String, socket: Socket) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(x509Certificates: Array<X509Certificate>, s: String, socket: Socket) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(x509Certificates: Array<X509Certificate>, s: String, sslEngine: SSLEngine) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(x509Certificates: Array<X509Certificate>, s: String, sslEngine: SSLEngine) {
+        }
+    })
+    return trustAllCerts
+}
+
