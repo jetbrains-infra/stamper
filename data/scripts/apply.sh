9dc2acee0cea79435ab2971f25477057cc060c4a
Nikita Ashikhmin <veoring@gmail.com>
2017-08-26 16:28:05 +0300

    Fix terraform local apply

--- data/scripts/apply.sh
+++ data/scripts/apply.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 echo Executing operation: terraform init
-docker run -i -v $PWD:$PWD --workdir=$PWD -v /var/run/docker.sock:/var/run/docker.sock $2 init -no-color
+terraform init -no-color
 
 echo Executing operation: terraform get
-docker run -i -v $PWD:$PWD --workdir=$PWD -v /var/run/docker.sock:/var/run/docker.sock $2  get -no-color
+terraform get -no-color
 
 echo Executing operation: terraform env new $1
-docker run -i -v $PWD:$PWD --workdir=$PWD -v /var/run/docker.sock:/var/run/docker.sock $2  workspace new $1 -no-color
+terraform workspace new $1 -no-color
 
 echo Executing operation: terraform env select $1
-docker run -i -v $PWD:$PWD --workdir=$PWD -v /var/run/docker.sock:/var/run/docker.sock $2  workspace select $1 -no-color
+terraform workspace select $1 -no-color
 
 echo Executing operation: terraform apply
-docker run -i -v $PWD:$PWD --workdir=$PWD -v /var/run/docker.sock:/var/run/docker.sock $2 apply -no-color
+terraform apply -no-color
