77e2c1a68c9a1839265e5fcc22df2142e3b8c955
nashikhmin <nashikhmin@naumen.ru>
2018-02-04 19:44:10 +0200

    TestContainers

--- stamper-backend/src/test/kotlin/ru/jetbrains/testenvrunner/selenium/AbstractSeleniumTest.kt
+++ stamper-backend/src/test/kotlin/ru/jetbrains/testenvrunner/selenium/AbstractSeleniumTest.kt
@@ -3,13 +3,71 @@ package ru.jetbrains.testenvrunner.selenium
 import com.codeborne.selenide.ElementsCollection
 import com.codeborne.selenide.Selenide.*
 import com.codeborne.selenide.SelenideElement
+import com.codeborne.selenide.WebDriverRunner
+import org.apache.commons.exec.CommandLine
+import org.apache.commons.exec.DefaultExecutor
+import org.apache.commons.exec.environment.EnvironmentUtils
 import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.ClassRule
 import org.openqa.selenium.By
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.testcontainers.containers.BrowserWebDriverContainer
+import java.io.File
+import java.net.NetworkInterface
+
+class KBrowserWebDriverContainer : BrowserWebDriverContainer<KBrowserWebDriverContainer>()
 
 /**
  * Abstract base class for Selenide testing
  */
 abstract class AbstractSeleniumTest {
+    companion object {
+        @ClassRule
+        @JvmField
+        var browserContainer: KBrowserWebDriverContainer = KBrowserWebDriverContainer()
+                .withDesiredCapabilities(DesiredCapabilities.chrome())
+                .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.SKIP, File("/home/nashikhmin/Documents"))
+
+        init {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver")
+            System.setProperty("selenide.browser", "Chrome")
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun after() {
+            close()
+        }
+
+        @BeforeClass
+        @JvmStatic
+        fun before() {
+            initWebDriver()
+            startVcn()
+        }
+
+        private fun initWebDriver() {
+            val driver = browserContainer.webDriver
+            WebDriverRunner.setWebDriver(driver)
+        }
+
+        private fun startVcn() {
+            val vncAddress = browserContainer.vncAddress.split("@")[1]
+            //потому что я рукожоп
+            val file = File(
+                    "src/test/resources/scripts/run_vcn.sh")
+            val command = CommandLine(file.absolutePath)
+            command.addArgument(vncAddress)
+            val executor = DefaultExecutor()
+            executor.execute(command, EnvironmentUtils.getProcEnvironment())
+        }
+    }
+
+    private fun getHostAddress() = NetworkInterface.getNetworkInterfaces().toList().filter { it.name == "docker0" }.last().inetAddresses.toList().get(
+            1).hostAddress
+
+    fun getAppAddress() = "http://${getHostAddress()}:3000"
     fun get(xpath: String): SelenideElement {
         return `$`(By.xpath(xpath))
     }
@@ -22,29 +80,17 @@ abstract class AbstractSeleniumTest {
         return `$$`(By.xpath(xpathSelector))
     }
 
-    companion object {
-        init {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver")
-            System.setProperty("selenide.browser", "Chrome")
-        }
-
-        @AfterClass
-        @JvmStatic
-        fun after() {
-            close()
-        }
-    }
-
     /**
      * Go to main page of the App
      */
-    protected fun openMainPage() = open("http://localhost:8080")
+    protected fun openMainPage() {
+        open(getAppAddress())
+    }
 
     /**
      * Go to run Stack form
      */
-    protected fun openRunStack() = open("http://127.0.0.1:3000/template/mysql/run")
-
+    protected fun openRunStack() = open("${getAppAddress()}/template/mysql/run")
 
     /**
      * Login to app by Google oauth2
